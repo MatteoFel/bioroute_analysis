f5b6496ec39537175eff70effdc3ebbe2cad9eeb
MatteoFel <82516091+MatteoFel@users.noreply.github.com>
2022-08-11 12:06:56 +0200

    Add files via upload

---
+++ bioroute/src/main/java/matteo/bioroute/examples/adrian/RunMichelEmmaFrequencies.java
@@ -0,0 +1,40 @@
+package matteo.bioroute.examples.adrian;
+
+import floetteroed.bioroute.BiorouteRunner;
+import floetteroed.bioroute.analysis.AnalysisRunner;
+import floetteroed.utilities.visualization.NetvisFromFileRunner;
+
+/**
+ * 
+ * @author Gunnar Flötteröd
+ *
+ * a copy of Gunnar's example, adapted to experiment with other types of analysis
+ * configurations.
+ *
+ */
+public class RunMichelEmmaFrequencies {
+
+	public static void main(String[] args) {
+		
+		//final String testdataPath = "./testdata/MichelEmmaNetwork/";
+		final String testdataPath = "/Users/matteofelder/Documents/IVT/bioroute/MichelEmmaNetwork/";
+		final String biorouteConfig = testdataPath + "config.xml";
+		final String netvisConfig = testdataPath + "vis-config.xml";
+		final String resultFile = testdataPath + "frequencies.xml";
+
+		BiorouteRunner.main(new String[] { biorouteConfig });
+
+		AnalysisRunner.main(new String[] { "FREQUENCIES", "-CONFIGFILE",
+				biorouteConfig, "-RESULTFILE", resultFile, "-totals", "true"});
+		
+//		AnalysisRunner.main(new String[] { "VISUAL", "-CONFIGFILE",
+//				biorouteConfig, "-VISCONFIGFILE", netvisConfig, "-VISDATAFILE",
+//				testdataPath + "vis-data.xml" });
+
+		// set link with to 250 to see something meaningful
+		NetvisFromFileRunner.main(new String[] { netvisConfig });		
+
+		
+	}
+	
+}
---
+++ bioroute/src/main/java/matteo/bioroute/examples/adrian/run_gzh_0_100.java
@@ -0,0 +1,37 @@
+package matteo.bioroute.examples.adrian;
+
+import floetteroed.bioroute.BiorouteRunner;
+import floetteroed.bioroute.analysis.AnalysisRunner;
+import floetteroed.utilities.visualization.NetvisFromFileRunner;
+
+/**
+ * 
+ * @author Gunnar Flötteröd
+ * adapted by Matteo Felder
+ *
+ */
+public class run_gzh_0_100 {
+
+	public static void main(String[] args) {
+		
+		final String testdataPath = "C:\\Users\\mfelder\\Documents\\bioroute\\";
+		final String biorouteConfig = testdataPath + "bioroute_path_and_config_files\\ebike\\config_0_100.xml";
+//		final String netvisConfig = testdataPath + "vis-config.xml";
+//		final String resultFile = testdataPath + "frequencies_0_100.xml";
+				
+		BiorouteRunner.main(new String[] { biorouteConfig });
+		
+//		AnalysisRunner.main(new String[] { "FREQUENCIES", "-CONFIGFILE",
+//				biorouteConfig, "-RESULTFILE", resultFile, "-totals", "true"});
+		
+//		AnalysisRunner.main(new String[] { "VISUAL", "-CONFIGFILE",
+//				biorouteConfig, "-VISCONFIGFILE", netvisConfig, "-VISDATAFILE",
+//				testdataPath + "vis-data.xml" });
+
+		// set link with to 250 to see something meaningful
+//		NetvisFromFileRunner.main(new String[] { netvisConfig });		
+
+		
+	}
+	
+}
